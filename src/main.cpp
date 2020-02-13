74ca7925d0ec24b1f0faa5f640e7b513b73ed5d0
kikke88 <kikke88@yandex.ru>
2020-02-13 18:12:17 +0300

    +arr of ptr

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,12 @@
 #include <iostream>
 #include <utility>
 #include <cstdlib>
+#include <vector>
+#include <functional>
 
 
 #include <fftw3-mpi.h>
 
-double b1_func( const double x1,
-				const double x2,
-				const double x3) {
-	return std::exp(std::sin(x1 + 2 * x2 - x3));
-}
-
-double b2_func( const double x1,
-				const double x2,
-				const double x3) {
-	return std::exp(std::cos(-8 * x1 + x2 - 9 * x3));
-}
-
-double b3_func( const double x1,
-				const double x2,
-				const double x3) {
-	return std::exp(std::sin(x1 + -3 * x3 - 2 * x3));
-}
 
 struct Task_features {
 	const ptrdiff_t N;
@@ -166,20 +151,40 @@ auto field_energy_fourie(	const fftw_complex *ptr_1,
 							const fftw_complex *ptr_3,
 							const Task_features& info) {
 	double energy_real = 0., energy_imag = 0.;
+	// for (ptrdiff_t i = 0; i < info.local_n0; ++i) {
+	// 	for (ptrdiff_t j = 0; j < info.N; ++j) {
+	// 		ptrdiff_t idx = (i * info.N + j) * (info.N / 2 + 1);
+	// 		energy_real += 0.5 * (	ptr_1[idx][0] * ptr_1[idx][0] - ptr_1[idx][1] * ptr_1[idx][1] +
+	// 								ptr_2[idx][0] * ptr_2[idx][0] - ptr_2[idx][1] * ptr_2[idx][1] +
+	// 								ptr_3[idx][0] * ptr_3[idx][0] - ptr_3[idx][1] * ptr_3[idx][1]);
+	// 		energy_imag += (ptr_1[idx][0] * ptr_1[idx][1] +
+	// 						ptr_2[idx][0] * ptr_2[idx][1] +
+	// 						ptr_3[idx][0] * ptr_3[idx][1]);
+	// 		for (ptrdiff_t k = 1; k < info.RANGE_RIGHT; ++k) { //  [1, N/2 + 1)
+	// 			++idx;
+	// 			energy_real += (ptr_1[idx][0] * ptr_1[idx][0] - ptr_1[idx][1] * ptr_1[idx][1] +
+	// 							ptr_2[idx][0] * ptr_2[idx][0] - ptr_2[idx][1] * ptr_2[idx][1] +
+	// 							ptr_3[idx][0] * ptr_3[idx][0] - ptr_3[idx][1] * ptr_3[idx][1]);
+	// 			energy_imag += 2 * (ptr_1[idx][0] * ptr_1[idx][1] +
+	// 								ptr_2[idx][0] * ptr_2[idx][1] +
+	// 								ptr_3[idx][0] * ptr_3[idx][1]);
+	// 		}
+	// 	}
+	// }
 	for (ptrdiff_t i = 0; i < info.local_n0; ++i) {
 		for (ptrdiff_t j = 0; j < info.N; ++j) {
 			ptrdiff_t idx = (i * info.N + j) * (info.N / 2 + 1);
-			energy_real += 0.5 * (	ptr_1[idx][0] * ptr_1[idx][0] - ptr_1[idx][1] * ptr_1[idx][1] +
-									ptr_2[idx][0] * ptr_2[idx][0] - ptr_2[idx][1] * ptr_2[idx][1] +
-									ptr_3[idx][0] * ptr_3[idx][0] - ptr_3[idx][1] * ptr_3[idx][1]);
+			energy_real += 0.5 * (	ptr_1[idx][0] * ptr_1[idx][0] +
+									ptr_2[idx][0] * ptr_2[idx][0] +
+									ptr_3[idx][0] * ptr_3[idx][0] );
 			energy_imag += (ptr_1[idx][0] * ptr_1[idx][1] +
 							ptr_2[idx][0] * ptr_2[idx][1] +
 							ptr_3[idx][0] * ptr_3[idx][1]);
 			for (ptrdiff_t k = 1; k < info.RANGE_RIGHT; ++k) { //  [1, N/2 + 1)
 				++idx;
-				energy_real += (ptr_1[idx][0] * ptr_1[idx][0] - ptr_1[idx][1] * ptr_1[idx][1] +
-								ptr_2[idx][0] * ptr_2[idx][0] - ptr_2[idx][1] * ptr_2[idx][1] +
-								ptr_3[idx][0] * ptr_3[idx][0] - ptr_3[idx][1] * ptr_3[idx][1]);
+				energy_real += (ptr_1[idx][0] * ptr_1[idx][0] +
+								ptr_2[idx][0] * ptr_2[idx][0] +
+								ptr_3[idx][0] * ptr_3[idx][0] );
 				energy_imag += 2 * (ptr_1[idx][0] * ptr_1[idx][1] +
 									ptr_2[idx][0] * ptr_2[idx][1] +
 									ptr_3[idx][0] * ptr_3[idx][1]);
@@ -241,14 +246,30 @@ void output_of_large_abs_value_fourie_coef(	const fftw_complex *ptr,
 	return;
 }
 
-int main(int argc, char *argv[]) {
-	const int power_of_two = std::atoi(argv[1]);
-	const ptrdiff_t N = 1 << power_of_two;
+void fill_real(double* vec[3], const Task_features& info) {
+	std::vector<std::function<double(const ptrdiff_t, const ptrdiff_t, const ptrdiff_t)>> fill_real_functions;
+	fill_real_functions.push_back([](const ptrdiff_t i, const ptrdiff_t j, const ptrdiff_t k) {
+		return std::sin(0.3 * i - 1 * j + k);
+	});
+	fill_real_functions.push_back([](const ptrdiff_t i, const ptrdiff_t j, const ptrdiff_t k) {
+		return std::cos(-1 * i - 0.1 * j - k);
+	});
+	fill_real_functions.push_back([](const ptrdiff_t i, const ptrdiff_t j, const ptrdiff_t k) {
+		return std::sin(i + k);
+	});
 
-	fftw_plan forward_plan, backward_plan;
-	double *rin;
-	fftw_complex *cout;
-	ptrdiff_t alloc_local, local_n0, local_0_start, i, j, k;
+	for (ptrdiff_t q = 0; q < 3; ++q)
+		for (ptrdiff_t i = 0; i < info.local_n0; ++i)
+			for (ptrdiff_t j = 0; j < info.N; ++j)
+				for (ptrdiff_t k = 0; k < info.N; ++k)
+					vec[q][(i * info.N + j) * (2 * (info.N / 2 + 1)) + k] = fill_real_functions[q](i, j, k);
+
+
+	return;
+}
+
+
+int main(int argc, char *argv[]) {
 /*
 	double 	*magnetic_field_1_real, *magnetic_field_2_real, *magnetic_field_3_real,
 			*velocity_field_1_real, *velocity_field_2_real, *velocity_field_3_real,
@@ -260,82 +281,75 @@ int main(int argc, char *argv[]) {
 					*rotor_1_complex,			*rotor_2_complex,			*rotor_3_complex,
 					*intermediate_field;
 */
+//  Task1
+	const int power_of_two = std::atoi(argv[1]);
+	const ptrdiff_t N = 1 << power_of_two;
+
 	MPI_Init(&argc, &argv);
 	int rank, size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	fftw_mpi_init();
 
-
+	fftw_plan forward_plan[3], backward_plan[3];
+	double* vec_r[3];
+	fftw_complex* vec_c[3];
+	ptrdiff_t alloc_local, local_n0, local_0_start;
 	alloc_local = fftw_mpi_local_size_3d(N, N, N / 2 + 1, MPI_COMM_WORLD, &local_n0, &local_0_start);
 
 	const Task_features info{N, 0, std::acos(-1) * 2, alloc_local, local_n0, local_0_start, rank, size};
 
-	rin = fftw_alloc_real(2 * alloc_local);
-	cout = fftw_alloc_complex(alloc_local);
+	for (int q = 0; q < 3; ++q) {
+		vec_r[q] = fftw_alloc_real(2 * alloc_local);
+		vec_c[q] = fftw_alloc_complex(alloc_local);
+		forward_plan[q]  = fftw_mpi_plan_dft_r2c_3d(N, N, N, vec_r[q], vec_c[q], MPI_COMM_WORLD, FFTW_MEASURE);
+		backward_plan[q] = fftw_mpi_plan_dft_c2r_3d(N, N, N, vec_c[q], vec_r[q], MPI_COMM_WORLD, FFTW_MEASURE);
+	}
 
-	forward_plan = fftw_mpi_plan_dft_r2c_3d(N, N, N, rin, cout, MPI_COMM_WORLD, FFTW_MEASURE);
-	backward_plan = fftw_mpi_plan_dft_c2r_3d(N, N, N, cout, rin, MPI_COMM_WORLD, FFTW_MEASURE);
-
-	for (i = 0; i < local_n0; ++i)
-		for (j = 0; j < N; ++j)
-			for (k = 0; k < N; ++k)
-				rin[(i * N + j) * (2 * (N / 2 + 1)) + k] = std::sin(i + j * k) + std::cos(i - k);
-
-	for (i = 0; i < local_n0; ++i)
-		for (j = 0; j < N; ++j)
-			for (k = 0; k < N; ++k)
-				std::cout << rin[(i * N + j) * (2 * (N / 2 + 1)) + k] << std::endl;
+	fill_real(vec_r, info);
 
 	std::cout << 'p' << '\n';
-	std::cout << field_energy_phi(rin, rin, rin, info) << '\n';
-	fftw_execute(forward_plan);
-	for (i = 0; i < local_n0; ++i)
-		for (j = 0; j < N; ++j)
-			for (k = 0; k < (N / 2 + 1); ++k)
-				cout[(i * N + j) * (N / 2 + 1) + k][0] /= N * std::sqrt(N);
-				cout[(i * N + j) * (N / 2 + 1) + k][1] /= N * std::sqrt(N);
+	std::cout << field_energy_phi(vec_r[0], vec_r[1], vec_r[2], info) << '\n';
+	fftw_execute(forward_plan[0]);
+	fftw_execute(forward_plan[1]);
+	fftw_execute(forward_plan[2]);
 
-	const auto [real, imag] = field_energy_fourie(cout, cout, cout, info);
+	for (int q = 0; q < 3; ++q)
+		for (ptrdiff_t i = 0; i < local_n0; ++i)
+			for (ptrdiff_t j = 0; j < N; ++j)
+				for (ptrdiff_t k = 0; k < (N / 2 + 1); ++k) {
+					vec_c[q][(i * N + j) * (N / 2 + 1) + k][0] /= N * std::sqrt(N);
+					vec_c[q][(i * N + j) * (N / 2 + 1) + k][1] /= N * std::sqrt(N);
+				}
+
+	const auto [real, imag] = field_energy_fourie(vec_c[0], vec_c[1], vec_c[2], info);
 	std::cout << "f" << '\n';
 	std::cout << real << ' ' << imag << '\n';
-	for (i = 0; i < local_n0; ++i)
-		for (j = 0; j < N; ++j)
-			for (k = 0; k < (N / 2 + 1); ++k)
-				std::cout << cout[(i * N + j) * (N / 2 + 1) + k][0] << ' ' << cout[(i * N + j) * (N / 2 + 1) + k][1] << std::endl;
+	// for (int q = 0; q < 3; ++q) {
+	// 	std::cout << "##" << std::endl;
+	// 	for (ptrdiff_t i = 0; i < local_n0; ++i)
+	// 		for (ptrdiff_t j = 0; j < N; ++j)
+	// 			for (ptrdiff_t k = 0; k < (N / 2 + 1); ++k)
+	// 				std::cout << vec_c[q][(i * N + j) * (N / 2 + 1) + k][0] << ' ' << vec_c[q][(i * N + j) * (N / 2 + 1) + k][1] << std::endl;
+	// }
+
 	//derivative_of_function(cout, info, 0);
 
-	fftw_execute(backward_plan);
-	for (i = 0; i < local_n0; ++i)
-		for (j = 0; j < N; ++j)
-			for (k = 0; k < N; ++k)
-				rin[(i * N + j) * (2 * (N / 2 + 1)) + k] /= N * sqrt(N);
-
-	std::cout << field_energy_phi(rin, rin, rin, info) << '\n';
-
-
-	return 0;
+	// fftw_execute(backward_plan);
 	// for (i = 0; i < local_n0; ++i)
 	// 	for (j = 0; j < N; ++j)
 	// 		for (k = 0; k < N; ++k)
-	// 			std::cout << cout[(i * N + j) * (N / 2 + 1) + k][0] << ' ' << cout[(i * N + j) * (N / 2 + 1) + k][1] << std::endl;
-	//derivative_of_function(cout, info, 0);
+	// 			rin[(i * N + j) * (2 * (N / 2 + 1)) + k] /= N * sqrt(N);
+	//
+	// std::cout << field_energy_phi(rin, rin, rin, info) << '\n';
 
-	std::cout << " --- " << '\n';
-	// for (i = 0; i < local_n0; ++i)
-	// 	for (j = 0; j < N; ++j)
-	// 		for (k = 0; k < N; ++k)
-	// 			std::cout << cout[(i * N + j) * (N / 2 + 1) + k][0] << ' ' << cout[(i * N + j) * (N / 2 + 1) + k][1] << std::endl;
-	for (i = 0; i < local_n0; ++i)
-		for (j = 0; j < N; ++j)
-			for (k = 0; k < N; ++k)
-				std::cout << rin[(i * N + j) * (2 * (N / 2 + 1)) + k] / 8 << std::endl;
+	for (int q = 0; q < 3; ++q) {
+		fftw_free(vec_r[q]);
+		fftw_free(vec_c[q]);
+		fftw_destroy_plan(forward_plan[q]);
+		fftw_destroy_plan(backward_plan[q]);
+	}
 
-
-	fftw_free(rin);
-	fftw_free(cout);
-	fftw_destroy_plan(forward_plan);
-	fftw_destroy_plan(backward_plan);
 
 	MPI_Finalize();
 
