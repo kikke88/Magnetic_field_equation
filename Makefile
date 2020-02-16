3c1d089eec5638e29516bd6dde5ede4678547ffd
kikke88 <kikke88@yandex.ru>
2020-02-16 21:03:30 +0300

    almost ready

--- Makefile
+++ Makefile
@@ -1,26 +1,32 @@
 OPT 		=	-O2 -std=c++1z
 FFTW_FLAGS 	=	-lfftw3_mpi -lfftw3 -lm
+INC			=	-I ./include/
 
 all: task1 task2
 
 task1: bin/task1
 task2: bin/task2
 
-bin/task1: build/task1.o Makefile
+bin/task1: build/task1.o
 	mkdir -p bin
 	mpic++ -o $@ $(OPT) $< $(FFTW_FLAGS)
 
-build/task1.o: src/task1.cpp Makefile
+build/task1.o: src/task1.cpp
 	mkdir -p build
 	mpic++ -o $@ $(OPT) -c $< $(FFTW_FLAGS)
 
-bin/task2: build/task2.o Makefile
+
+bin/task2: build/task2.o build/Field.o
 	mkdir -p bin
-	mpic++ -o $@ $(OPT) $< $(FFTW_FLAGS)
+	mpic++ -o $@ $(OPT) $(INC) $^ $(FFTW_FLAGS)
 
-build/task2.o: src/task2.cpp Makefile
+build/task2.o: src/task2.cpp
 	mkdir -p build
-	mpic++ -o $@ $(OPT) -c $< $(FFTW_FLAGS)
+	mpic++ -o $@ $(OPT) $(INC) -c $< $(FFTW_FLAGS)
+
+build/Field.o: src/Field.cpp include/Field.hpp
+	mkdir -p build
+	mpic++ -o $@ $(OPT) $(INC) -c $< $(FFTW_FLAGS)
 
 clean:
 	rm -rf build
---
+++ include/Field.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+enum class Modes {
+	ALL_COMPONENTS,
+	ALL_COMPLEX_COMPONENTS,
+	ALL_REAL_COMPONENTS,
+	ONE_COMPLEX_COMPONENT
+};
+
+const double EPSILON = 1e-10;
+
+class Field {
+private:
+	double* vec_r[3];
+	fftw_complex* vec_c[3];
+	fftw_plan forward_plan[3], backward_plan[3];
+
+	const Modes mode;
+
+	const ptrdiff_t N;
+	const double TAU, NIU;
+	const ptrdiff_t INDEX_LEFT, INDEX_RIGHT;
+	const double RANGE_LEFT, RANGE_RIGHT;
+
+	const ptrdiff_t alloc_local, local_n0, local_0_start;
+
+	double *indeces;
+	const double NORMALIZATION_CONSTANT;
+	int rank, size;
+
+	void cross_product(const Field&, const Field&);
+
+	void divergence(const Field&);
+
+public:
+	Field(  const Modes, const ptrdiff_t, const double, const double,
+			const double, const double,
+			const ptrdiff_t, const ptrdiff_t, const ptrdiff_t,
+			const int, const int);
+
+	~Field();
+
+	void forward_transformation();
+
+	void backward_transformation();
+
+	void correction(Field&);
+
+	void rotor(const Field&, const Field&);
+
+	void do_step(const Field&, Field&);
+
+	double energy_phi() const;
+
+	double energy_fourie() const;
+
+	void fill_velocity_field();
+
+	void fill_magnetic_field();
+};
