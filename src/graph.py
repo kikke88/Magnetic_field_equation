67d0489df7c3578bbd5a08a6a1bd30a4a43ee75d
kikke88 <kikke88@yandex.ru>
2020-03-01 16:17:35 +0300

    correct graph for eta = 0.1

--- src/graph.py
+++ src/graph.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.loadtxt('eta1.data')
+data = np.loadtxt('mpisublSpectrum.data')
 
 fig = plt.figure()
 
-# x = np.array([np.float64(x) for x in range(0, 100000)])
-# y = np.exp(0.000097 * x, dtype = np.float64)
-# y += 10**4
-# plt.plot(y, label='exp^0.000097*x, 1 step in x dim ~ 10^-4 s')
+x = np.array([np.float64(x) for x in range(0, 160000)])
+y = np.exp(0.000055 * x, dtype = np.float64)
+#y += 10**4
+plt.plot(y, label='exp^0.000055*x, 1 step in x dim ~ 10^-4 s')
 
-plt.plot(data, label = 'η = 1, τ = 10^-4')
+plt.plot(data, label = 'η = 0.1, τ = 10^-4')
 plt.yscale('log')
 
 plt.title('Magnetic field energies (log scale)')
